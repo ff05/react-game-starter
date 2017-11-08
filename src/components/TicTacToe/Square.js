498ef50eda96b0a0abcf239ecd22593c4cb8b148
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-11-08 12:14:14 +0100

    Create components GameTTT, Board, Square, and Made squares display on game page

---
+++ src/components/TicTacToe/Square.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class Square extends PureComponent {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+export default Square
