305c7d0e3b04c7973ee4261a25df34fa851371a2
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-11-08 16:12:31 +0100

    add calculate of winner, and X's and O's

--- src/components/TicTacToe/Square.js
+++ src/components/TicTacToe/Square.js
@@ -1,10 +1,14 @@
 import React, { PureComponent } from 'react'
 import Board from './Board'
 import FlatButton from 'material-ui/FlatButton'
+import './Square.css'
 
 function Square(props) {
   return (
-    <FlatButton className="square" onClick={props.onClick} label={props.value} />
+
+    <FlatButton className="square" onClick={props.onClick} label=" " >
+      {props.value}
+    </FlatButton>
   );
 }
 
