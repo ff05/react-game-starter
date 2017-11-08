d00e6d1457bec7adf0a81461c09cecb642f2338f
frankfil <frank.filius@gmail.com>
2017-11-08 17:27:46 +0100

    add updating action

--- src/actions/games/subscribe.js
+++ src/actions/games/subscribe.js
@@ -1,4 +1,34 @@
+import API from '../../api/client'
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  LOAD_ERROR,
+  LOAD_SUCCESS
+} from '../loading'
+
 export const GAME_CREATED = 'GAME_CREATED'
 export const GAME_UPDATED = 'GAME_UPDATED'
 export const GAME_REMOVED = 'GAME_REMOVED'
 export const GAME_PLAYERS_UPDATED = 'GAME_PLAYERS_UPDATED'
+
+const api = new API()
+
+export const updating = (game) => {
+  return (dispatch) => {
+    dispatch({ type: APP_LOADING })
+
+    api.patch(`/games/${game._Id}`, [1])
+      .then(() => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+      })
+
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
+        })
+      })
+  }
+}
