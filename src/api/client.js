6ba5488943ca1eee3b916bbc1f7ec43f3f491ec9
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-11-10 12:31:19 +0100

    fixed typo

--- src/api/client.js
+++ src/api/client.js
@@ -9,7 +9,7 @@ export default class ApiClient {
 
   constructor(host, options = {}) {
      this.host = process.env.NODE_ENV === 'production'
-       ? 'https://taller-huntress.codaisseur.cloud' // WITHOUT the / !!!
+       ? 'https://tender-yardwork.codaisseur.cloud' // WITHOUT the / !!!
        : (host || 'http://localhost:3030')
 
   // Authenticate and store the token
